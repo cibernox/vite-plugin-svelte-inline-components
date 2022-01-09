d0fc41e93c94ea6f955c130e837ae0d191e0cd84
Miguel Camba <miguel.camba@gmail.com>
2022-01-09 22:28:45 +0100

    Initial implementation.
    It works by encoding the source code of the components as a base64 strings and adding it to the id of the virtual module.
    The the vite plugin decodes that base64 string when serving that virtual module. From there the regular svelte compiler takes
    over and it all works.
    
    The upside of this approach is that is very simple as it doesn't require cross-worker comunication.
    The downside is that the id of the virtual modules are obnoxiously long, of the likes of:
    
    ```
    virtual:inline-svelte:CiAgICAgIDxzY3JpcHQ+aW1wb3J0IERlZmluaXRpb25FbnRyeSBmcm9tICckbGliL0RlZmluaXRpb25FbnRyeS5zdmVsdGUnOzwvc2NyaXB0PgogICAgICA8RGVmaW5pdGlvbkVudHJ5IGJhY2tncm91bmQ9ImdyYXkiPgogICAgICAgIDxzdmVsdGU6ZnJhZ21lbnQgc2xvdD0iZHQiPkknbSB0aGUgZGVzY3JpcHRpb24gdGVybTwvc3ZlbHRlOmZyYWdtZW50PgogICAgICAgIDxzdmVsdGU6ZnJhZ21lbnQgc2xvdD0iZGQiPkknbSB0aGUgZGVzY3JpcHRpb24gZGVmaW5pdGlvbjwvc3ZlbHRlOmZyYWdtZW50PiAgICAKICAgICAgPC9EZWZpbml0aW9uRW50cnk+CiAgICA=.svelte
    ```
    They can be even longer. I don't know if there might be a limit that extremely long examples might encounter, so far it doesn't seem to be. The URL spec certainly doesn't define a limit for the length of URLs.
    
    Another downside is that it requires users repeat the script with the import statements on every test, which gets verbose and repetitive.
    Some upside is that since imports live in these ad-hoc components, we don't have to worry about ESLint of typescript thinking an import statement is unused.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-async function hash(message) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(message);
-  const hashBuffer = await crypto.subtle.digest('SHA-1', data);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');  
+function encodeBase64(str) {
+  let buf = Buffer.from(str, 'utf8');
+  return buf.toString('base64');
 }
 
-const CODE_HASH_MAPPING = {};
-
 export async function svelte([code]) {
-  const id = `virtual:inline-svelte:${await hash(code)}.svelte`
-  CODE_HASH_MAPPING[id] = code;
+  const id = `virtual:inline-svelte:${encodeBase64(code)}.svelte`
   return import(id);
 }
 
@@ -25,8 +19,10 @@ export default function vitePluginSvelteInlineComponents() {
     },
 
     load(id) {
-      if (id.startsWith('virtual:inline-svelte') && CODE_HASH_MAPPING.hasOwnProperty(id)) {
-        return CODE_HASH_MAPPING[id];
+      if (id.startsWith('virtual:inline-svelte')) {
+        let base64ComponentSource = id.split(/virtual:inline-svelte:/)[1].slice(0, -7);
+        let buf = Buffer.from(base64ComponentSource, 'base64');
+        return buf.toString('utf8');
       }
     },
   }
